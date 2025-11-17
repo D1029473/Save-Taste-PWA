85a2f81120c2236b817cef7587682c2ab8e8aea4
D1029473 <dmir1029384756@gmail.com>
2025-11-17 08:47:15 +0100

    Add service worker for caching resources

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE = "nutriscout-cache-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
